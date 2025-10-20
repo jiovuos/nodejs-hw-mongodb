7da54d76ef2fb7ad2582abd862927d61f3da7444
Volodymyr <voldemar.v.lysenko@gmail.com>
2025-10-20 20:59:31 +0300

    Clean main branch

--- src/routes/contacts.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAllContactsController, getContactByIdController } from '../controllers/contactsController.js';
-
-const router = Router();
-
-router.get('/', getAllContactsController);
-router.get('/:contactId', getContactByIdController);
-
-export default router;
--- src/sevices/contacts.js
+++
@@ -1,9 +0,0 @@
-import { Contact } from '../models/Contact.js';
-
-export const getAllContacts = async () => {
-  return await Contact.find();
-};
-
-export const getContactById = async (id) => {
-  return await Contact.findById(id);
-};
