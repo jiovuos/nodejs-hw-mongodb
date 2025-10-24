4c4ef788e56faa5b519d2db2e125ce9a04e05a5f
Volodymyr <voldemar.v.lysenko@gmail.com>
2025-10-24 19:10:40 +0300

    fix response

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -4,6 +4,8 @@ export const getAllContactsController = async (req, res, next) => {
   try {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
+      message: "Successfully found contacts!",
       data: contacts
     });
   } catch (error) {
@@ -15,11 +17,18 @@ export const getContactByIdController = async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
+
     if (!contact) {
-      return res.status(404).json({ message: "Contact not found" });
+      return res.status(404).json({
+        status: 404,
+        message: "Contact not found",
+        data: null
+      });
     }
 
     res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
       data: contact
     });
   } catch (error) {
